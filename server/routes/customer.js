53c30a5c3742a8d0c04488404b82e4d551d42db6
John Wu <john.wu@mail.mcgill.ca>
2017-09-16 06:22:34 -0400

    Adding video posting endpoint

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -1,7 +1,19 @@
+var fs = require('fs');
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, 'video.mp4')
+    }
+});
+var upload = multer({ storage: storage });
+
 module.exports = function (app) {
 
-    app.post('/customer', function (req, res) {
-        res.send('Customer posted')
+    app.post('/customer', upload.single('video'), function (req, res) {
+        res.send('Video received')
     })
 
 }
