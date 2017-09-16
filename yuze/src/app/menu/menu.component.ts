7011c16a9819b541aed4dc71e9f6f84286bf9c05
tiff-wang <wangtiffany@gmail.com>
2017-09-16 17:47:57 -0400

    added toggling the selected / unselected button in menu

--- yuze/src/app/menu/menu.component.ts
+++ yuze/src/app/menu/menu.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
   }
+
+  changeStatus(event:Event):void {
+    if((<HTMLInputElement>event.target).classList.contains('selected')){
+      (<HTMLInputElement>event.target).classList.remove('selected');
+    }else{
+      (<HTMLInputElement>event.target).classList.add('selected');
+    }
+  }
+
+  // onHeroChange(event:Event):void {
+  //   Hero hero = heros.firstWhere(
+  //
+  //   // Hero hero = heros[int.parse((event.target as SelectElement).value)];
+  //   selectedHero = hero;
+  //   selectedHeroChange.add(hero);
+  // }
 
 }
