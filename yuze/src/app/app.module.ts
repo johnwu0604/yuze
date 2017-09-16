5f1000a42b95dfdfedc51dc45a8d9297e36e4456
tiff <wangtiffany@gmail.com>
2017-09-16 10:05:18 +0000

    added some dependencies

--- yuze/src/app/app.module.ts
+++ yuze/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+
 import { AppComponent } from './app.component';
+import { MenuModule } from './menu/menu.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonsModule.forRoot(),
+    MenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
