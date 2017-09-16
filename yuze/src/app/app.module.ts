c4794deeff86c99f82949bfa30ff53f3099348f1
tiff-wang <wangtiffany@gmail.com>
2017-09-16 15:18:03 -0400

    completed template for checkout page

--- yuze/src/app/app.module.ts
+++ yuze/src/app/app.module.ts
@@ -1,7 +1,6 @@
 //modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { MenuModule } from './menu/menu.module';
 import { LoginModule } from './login/login.module';
@@ -9,16 +8,18 @@ import { LoginModule } from './login/login.module';
 //components
 import { AppComponent } from './app.component';
 import { RecordRTCComponent } from './record-rtc/record-rtc.component';
-import {LoginComponent} from "./login/login.component";
+import { LoginComponent} from "./login/login.component";
 
 import { routing } from './app.routing';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     RecordRTCComponent,
-    LoginComponent
+    LoginComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
